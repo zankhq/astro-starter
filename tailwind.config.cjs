ba94eea7d29ec082fc9d7528f475fe9f2c3730d7
Giulio Zanchetta <zank@outlook.it>
2023-09-11 02:04:18 +0200

    feat: Update global styles and theme colors
    - Apply box-border to all elements in global.css
    - Add transition-all duration-300 to body in global.css
    - Change font family for sans in tailwind.config.cjs
    - Override primary and secondary colors in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,37 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
+				/** Change here the font family */
 				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				/** Override here your primary colors */
+				primary: {
+					50: "#fef1f7",
+					100: "#fee5f0",
+					200: "#fecce3",
+					300: "#ffa2cb",
+					400: "#fe68a7",
+					500: "#f83c86",
+					600: "#e91f64",
+					700: "#ca0c47",
+					800: "#a70d3b",
+					900: "#8b1034",
+					950: "#55021a",
+				},
+				secondary: {
+					50: "#f4f7f7",
+					100: "#e2ebeb",
+					200: "#c8d9d8",
+					300: "#a1bfbf",
+					400: "#739c9d",
+					500: "#588182",
+					600: "#4c6c6e",
+					700: "#425a5c",
+					800: "#3b4d4f",
+					900: "#354244",
+					950: "#222d2f",
+				},
 			},
 		},
 	},
