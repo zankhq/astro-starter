9914273985072744f360f0ddc5075285b89b0acf
zank <zank@outlook.it>
2023-09-16 23:25:03 +0200

    Refactor consts.ts for improved readability and maintainability
    - Removed unnecessary comments and blank lines
    - Updated variable names for clarity
    - Added type definition for Page object
    - Reorganized code sections

--- src/consts.ts
+++ src/consts.ts
@@ -2,25 +2,22 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website metadata
-
 export const SITE_URL: string = "https://example.com/";
 export const SITE_TITLE: string = "Astro Blog";
 export const SITE_DESCRIPTION: string = "Welcome to my website!";
 
 // Repo info
-
 export const REPO: string = "zankhq/astro-starter"; // TODO: Update dynamically
 export const DEFAULT_BRANCH: string = "main"; // TODO: Update dynamically
 
 // SEO metadata
-
 export const TWITTER_CREATOR: string = "@xxx";
 
 // Navigation
-
 type Page = {
 	title: string;
 	href: string;
+	children?: Page[];
 };
 
 export const PAGES: Page[] = [
@@ -39,7 +36,6 @@ export const PAGES: Page[] = [
 ];
 
 // i18n
-
 export const DEFAULT_LOCALE = "en";
 export const LOCALES = {
 	en: "en", // the `defaultLocale` value must present in `locales` keys
@@ -47,5 +43,4 @@ export const LOCALES = {
 };
 
 // Hosting
-
 export const HOSTING_SERVICE: "cloudflare" | "netlify" | "none" = "cloudflare";
