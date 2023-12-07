d2a98d32588b39da79bf51c3de634f767f6cac07
zank <zank@outlook.it>
2023-12-07 23:58:00 +0100

    Change output format from "server" to "hybrid" in astro.config.mjs
    - Update the output configuration in astro.config.mjs from "server" to "hybrid".

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ const locales = LOCALES;
 // https://astro.build/config
 export default defineConfig({
 	site: SITE_URL,
-	output: "server",
+	output: "hybrid",
 	trailingSlash: "always",
 	build: {
 		format: "directory",
