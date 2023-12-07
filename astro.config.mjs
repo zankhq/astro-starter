50a9d7884359d8b174ecc9ddf2d3b771431f09fc
zank <zank@outlook.it>
2023-12-07 23:35:40 +0100

    feat: Add Astro Sveltia CMS and Cloudflare adapter
    This commit adds the Astro Sveltia CMS package to handle content management in Astro projects. It also includes the Cloudflare adapter for deployment on the Cloudflare Workers platform.
    
    The following changes were made:
    - Added `sveltiaCMS()` to the build configuration
    - Added `cloudflare()` as the adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-import AstroPWA from "@vite-pwa/astro";
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import { DEFAULT_LOCALE, LOCALES, SITE_URL } from "./src/consts";
+import sveltiaCMS from "astro-sveltia-cms";
+import cloudflare from "@astrojs/cloudflare";
 
 const defaultLocale = DEFAULT_LOCALE;
 const locales = LOCALES;
 
-const prodEnv = import.meta.env.PROD;
-
 // https://astro.build/config
 export default defineConfig({
 	site: SITE_URL,
+	output: "hybrid",
 	trailingSlash: "always",
 	build: {
 		format: "directory",
@@ -36,55 +36,16 @@ export default defineConfig({
 				defaultLocale,
 			}),
 		}),
-		tailwind(),
+		sveltiaCMS(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
 		alpinejs(),
 		i18n({
 			locales,
 			defaultLocale,
 			exclude: ["pages/api/**/*", "pages/rss.xml.ts", "pages/[locale]/rss.xml.ts"],
 		}),
-		// Disable sw until figure out the problems
-		// AstroPWA({
-		// 	mode: prodEnv ? "production" : "development",
-		// 	base: "/",
-		// 	scope: "/",
-		// 	includeAssets: ["favicon.svg"],
-		// 	registerType: "autoUpdate",
-		// 	manifest: {
-		// 		name: "Astro starter",
-		// 		short_name: "Astro starter",
-		// 		theme_color: "#ffffff",
-		// 		icons: [
-		// 			{
-		// 				src: "pwa-192x192.png",
-		// 				sizes: "192x192",
-		// 				type: "image/png",
-		// 			},
-		// 			{
-		// 				src: "pwa-512x512.png",
-		// 				sizes: "512x512",
-		// 				type: "image/png",
-		// 			},
-		// 			{
-		// 				src: "pwa-512x512.png",
-		// 				sizes: "512x512",
-		// 				type: "image/png",
-		// 				purpose: "any maskable",
-		// 			},
-		// 		],
-		// 	},
-		// 	workbox: {
-		// 		navigateFallback: "/",
-		// 		globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
-		// 	},
-		// 	devOptions: {
-		// 		enabled: true,
-		// 		suppressWarnings: true,
-		// 		navigateFallbackAllowlist: [/^\//],
-		// 	},
-		// 	experimental: {
-		// 		directoryAndTrailingSlashHandler: true,
-		// 	},
-		// }),
 	],
+	adapter: cloudflare(),
 });
