ce593bfb1bae21950e75ffb6b87d25292c315656
zank <zank@outlook.it>
2024-02-08 23:06:07 +0100

    Updated Astro configuration
    Removed unused dependencies and adjusted the build settings in the Astro configuration file. The Sveltia CMS and Cloudflare packages were removed as they are no longer needed. Also, the 'output' property was deleted from the config object to streamline the build process.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import { DEFAULT_LOCALE, LOCALES, SITE_URL } from "./src/consts";
-import sveltiaCMS from "astro-sveltia-cms";
-import cloudflare from "@astrojs/cloudflare";
 
 const defaultLocale = DEFAULT_LOCALE;
 const locales = LOCALES;
@@ -14,7 +12,6 @@ const locales = LOCALES;
 // https://astro.build/config
 export default defineConfig({
 	site: SITE_URL,
-	output: "server",
 	trailingSlash: "always",
 	build: {
 		format: "directory",
@@ -47,5 +44,4 @@ export default defineConfig({
 			exclude: ["pages/api/**/*", "pages/rss.xml.ts", "pages/[locale]/rss.xml.ts"],
 		}),
 	],
-	adapter: cloudflare(),
 });
